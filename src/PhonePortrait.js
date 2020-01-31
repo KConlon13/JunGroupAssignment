a535e4615926a2a074c0f97b8274703cb347bcb4
KConlon13 <KyleConlon13@gmail.com>
2020-01-30 19:01:59 -0500

    added main body to the copy and additional styling to the CTA button

--- src/PhonePortrait.js
+++ src/PhonePortrait.js
@@ -1,17 +1,19 @@
 import React from "react";
 import './App.css';
-import TacoBellWhiteLogo from "./tacobellwhitelogo.png"
+// import TacoBellWhiteLogo from "./tacobellwhitelogo.png"
+import BelluminatiLogo from "./BelluminatiLogo.png"
 
 class PhonePortrait extends React.Component {
     // This component is for the iPhone 8 Portrait View
     render(){
         return(
             <div className="PhonePortrait">
-                <img className="Taco-Bell-Logo" src={TacoBellWhiteLogo} alt="Taco Bell White"/>
+                <img className="Taco-Bell-Logo" src={BelluminatiLogo} alt="Taco Bell White"/>
                 <h3 className="Portrait-Small-Title-Copy">Welcome To The</h3>
                 <h3 className="Portrait-Large-Title-Copy">BELLUMINATI</h3>
+                <h4 className="Portrait-Copy">Congratulations. You've discovered the secret world of the Belluminati where the dollar truly gets you more. Nothing here is too lavish or extravagant - as long as you have one dollar. Long live the power of the dollar.</h4>
                 <a href="https://www.tacobell.com/" target="_blank" rel="noopener noreferrer">
-                    <button class="ui inverted button">Uncover More Secrets Here</button>
+                    <button class="ui inverted button">UNCOVER MORE SECRETS HERE</button>
                 </a>
 
                 <div className="Social-Button-Group">
@@ -21,7 +23,7 @@ class PhonePortrait extends React.Component {
                         </button>
                     </a>
                     <a href="https://www.instagram.com/tacobell/?hl=en" target="_blank" rel="noopener noreferrer">
-                        <button class="ui circular instagram icon button">
+                        <button class="ui purple circular instagram icon button">
                             <i class="instagram icon"></i>
                         </button>
                     </a>
